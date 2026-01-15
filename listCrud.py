c8a66e3a69d2a13712f8c7f6a26c15f0b2a25e8e
dzrenata <renatavkksa@gmail.com>
2026-01-15 16:15:32 +0200

    refactored list CRUD to functions

---
+++ listCrud.py
@@ -0,0 +1,76 @@
+def load_default_data():
+    return [
+    {
+        "id": 1,
+        "name": "Vanilė",
+        "species": "šuo",
+        "birth_year": 2014
+    },
+    {
+        "id": 2,
+        "name": "Karamelė",
+        "species": "triušis",
+        "birth_year": 2023
+    },
+    {
+        "id": 3,
+        "name": "Lakis",
+        "species": "vėžlys",
+        "birth_year": 2025
+    }
+]
+
+def print_info():
+    print("------------------------------------------------------------")
+    print("1. Atvaizduoti augintinius")
+    print("2. Pridėti augintinį")
+    print("3. Redaguoti augintinį")
+    print("4. Šalinti augintinį")
+    print("5. Išeiti iš programos")
+    print("---------------------- Pasirinkite: -------------------------")
+
+def print_pets(pets):
+    for pet in pets:
+        print(f"{pet["id"]}. {pet["name"]} – {pet["species"]}, gimimo metai: {pet["birth_year"]}")
+
+def create_pet(pets, id_counter):
+    print("Augintinio pridėjimas")
+    print("Įveskite vardą:")
+    name = input()
+    print("Įveskite rūšį:")
+    species = input()
+    print("Įveskite gimimo metus:")
+    birth_year = int(input())
+    id_counter += 1
+    pet = {
+        "id": id_counter,
+        "name": name,
+        "species": species,
+        "birth_year": birth_year
+    }
+    pets.append(pet)
+    return id_counter
+
+def edit_pet(pets):
+    print("Augintinio redagavimas")
+    print("Įveskite augintinio ID:")
+    edit_id = input()
+    for pet in pets:
+        if edit_id == str(pet["id"]):
+            print(f"Redaguojamas: {pet["id"]}. {pet["name"]} – {pet["species"]}, gimimo metai: {pet["birth_year"]}")
+            print("Naujas vardas:")
+            pet["name"] = input()
+            print("Nauja rūšis:")
+            pet["species"] = input()
+            print("Nauji gimimo metai:")
+            pet["birth_year"] = int(input())
+
+def remove_pet(pets):
+    print("Augintinio šalinimas")
+    print("Įveskite augintinio ID:")
+    del_id = input()
+    for pet in pets:
+        if del_id == str(pet["id"]):
+            print(f"Šalinamas: {pet["id"]}. {pet["name"]} – {pet["species"]}, gimimo metai: {pet["birth_year"]}")
+            pets.remove(pet)
+            break
--- main.py
+++ main.py
@@ -1,86 +1,22 @@
-                                     # MY PETS CRUD (id, name, species, birth_year)
+from listCrud import *
+# MY PETS CRUD (id, name, species, birth_year)
 
-pets = [
-    {
-        "id": 1,
-        "name": "Vanilė",
-        "species": "šuo",
-        "birth_year": 2014
-    },
-    {
-        "id": 2,
-        "name": "Karamelė",
-        "species": "triušis",
-        "birth_year": 2023
-    },
-    {
-        "id": 3,
-        "name": "Lakis",
-        "species": "vėžlys",
-        "birth_year": 2025
-    }
-]
+pets = load_default_data()
 
 id_counter = 3
 
 while True:
-    print("------------------------------------------------------------")
-    print("1. Atvaizduoti augintinius")
-    print("2. Pridėti augintinį")
-    print("3. Redaguoti augintinį")
-    print("4. Šalinti augintinį")
-    print("5. Išeiti iš programos")
-    print("---------------------- Pasirinkite: -------------------------")
-
+    print_info()
     įvestis = input()
-
     match įvestis:
         case "1":
-            print("Augintinių sąrašas:")
-            for pet in pets:
-                print(f"{pet["id"]}. {pet["name"]} – {pet["species"]}, gimimo metai: {pet["birth_year"]}")
-
+            print_pets(pets)
         case "2":
-            print("Augintinio pridėjimas")
-            print("Įveskite vardą:")
-            name = input()
-            print("Įveskite rūšį:")
-            species = input()
-            print("Įveskite gimimo metus:")
-            birth_year = int(input())
-            id_counter += 1
-            pet = {
-                "id": id_counter,
-                "name": name,
-                "species": species,
-                "birth_year": birth_year
-            }
-            pets.append(pet)
-
+            id_counter = create_pet(pets, id_counter)
         case "3":
-            print("Augintinio redagavimas")
-            print("Įveskite augintinio ID:")
-            edit_id = input()
-            for pet in pets:
-                if edit_id == str(pet["id"]):
-                    print(f"Redaguojamas: {pet["id"]}. {pet["name"]} – {pet["species"]}, gimimo metai: {pet["birth_year"]}")
-                    print("Naujas vardas:")
-                    pet["name"] = input()
-                    print("Nauja rūšis:")
-                    pet["species"] = input()
-                    print("Nauji gimimo metai:")
-                    pet["birth_year"] = int(input())
-
+            edit_pet(pets)
         case "4":
-            print("Augintinio šalinimas")
-            print("Įveskite augintinio ID:")
-            del_id = input()
-            for pet in pets:
-                if del_id == str(pet["id"]):
-                    print(f"Šalinamas: {pet["id"]}. {pet["name"]} – {pet["species"]}, gimimo metai: {pet["birth_year"]}")
-                    pets.remove(pet)
-                    break
-
+            remove_pet(pets)
         case "5":
             print("Išeinama iš programos")
             break
